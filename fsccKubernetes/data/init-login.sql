9acad5efe0ad723df2389daa1cf081ec1fba71cc
Matthew Palmer <matthewpalmer9@outlook.com>
2021-11-23 09:24:57 -0600

    saving progress

---
+++ fsccKubernetes/data/init-login.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS login_db;
+
+USE login_db;
+CREATE TABLE IF NOT EXISTS users (
+    id INT,
+    email VARCHAR(50),
+    password VARCHAR(60),
+    UNIQUE(email)
+);
+
+insert into users (id, email, password) values (1, 'admin@developer.com', '$2a$10$PSutxF8kg65/RRh61gl1q.WI8kS.Gzd.rK0EDpR4QSqW7anpPgTfG');
